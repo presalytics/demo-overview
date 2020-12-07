3fb77677bc6e1555ce7406961d072ea35963a733
kevin-presalytics <50470667+kevin-presalytics@users.noreply.github.com>
2020-12-07 07:42:19 -0800

    Update update_template.py
    loops through all pages add adds TextReplace Widget

--- update_template.py
+++ update_template.py
@@ -24,22 +24,26 @@ def dummy_api_call_or_sql_query():
 # Turn the local copy of story outline, "story.yaml", into a python object
 outline: presalytics.StoryOutline = presalytics.StoryOutline.import_yaml('story.yaml')
 
-# Store the serialized widget that we are going to update in a python variable called "template_widget"
-template_widget = outline.pages[2].widgets[0]
-
 text_transform_metadata = dummy_api_call_or_sql_query()  # Simulates an API call / database query
 
-# Creates a new widget that dynamically updates the page as the result of dummy_api_call_or_sql_query() change
-new_widget = presalytics.OoxmlEditorWidget(
-    name="New Widget with filed in Text",  # A name -- can be any string that describes the widget
-    story_id=template_widget.data["story_id"],  # The story Id
-    object_ooxml_id=template_widget.data["object_ooxml_id"],  # A reference in the presalytics API to the slide we are updating
-    endpoint_map=presalytics.OoxmlEndpointMap.slide(),  # The type of object we are updating (Defines the endpoint for Presalytics API calls)
-    transform_class=presalytics.TextReplace,  # The action that we want this widget to take
-    transform_params=text_transform_metadata  # The metadata that the action uses to transform the Widget (e.g, from API call)
-)
+# Store the serialized widget that we are going to update in a python variable called "template_widget"
+for i in range(0, len(outline.pages)):
+        
+    template_widget = outline.pages[i].widgets[0]
 
-outline.pages[2].widgets[0] = new_widget.serialize()  # update the python representation of the story outline
+    # Creates a new widget that dynamically updates the page as the result of dummy_api_call_or_sql_query() change
+    new_widget = presalytics.OoxmlEditorWidget(
+        name="New Widget with filed in Text",  # A name -- can be any string that describes the widget
+        story_id=template_widget.data["story_id"],  # The story Id
+        object_ooxml_id=template_widget.data["object_ooxml_id"],  # A reference in the presalytics API to the slide we are updating
+        endpoint_map=presalytics.OoxmlEndpointMap.slide(),  # The type of object we are updating (Defines the endpoint for Presalytics API calls)
+        transform_class=presalytics.TextReplace,  # The action that we want this widget to take
+        transform_params=text_transform_metadata  # The metadata that the action uses to transform the Widget (e.g, from API call)
+    )
+
+    outline.pages[i].widgets[0] = new_widget.serialize()  # update the python representation of the story outline
+
+    del template_widget
 
 outline.export_yaml('story.yaml')  # write the outline to the story.yaml file
 
